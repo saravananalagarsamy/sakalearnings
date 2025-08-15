61f88784dce0467451919ad76576226990c13ab6
sa ka <saravananalagarsamy@yahoo.com>
2025-08-14 22:21:22 -0700

    data types

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,4 +1,19 @@
-fname = "saka"
-lname = "ku"
-print(welcome:= f"Welcome {fname} {lname} to the world of Python programming!")
-print(welcome)
+number: int = 10
+string: str = "Hello, World!"
+float: float = 3.14
+boolean: bool = True
+
+list_example: list = [1, 2, 3, 4, 5]
+tuple_example: tuple = (1, 2, 3, 4, 5)
+set_example: set = {1, 2, 3, 4, 5}
+dict_example: dict = {"key1": "value1", "key2": "value2"}
+
+print(f"Number: {number}")
+print(f"String: {string}")
+print(f"Float: {float}")
+print(f"Boolean: {boolean}")
+
+print("List Example:", list_example)
+print("Tuple Example:", tuple_example)
+print("Set Example:", set_example)
+print("Dictionary Example:", dict_example)
