a7c5a46ddfe57182dd44fc74f7ce57a047fff58d
sa ka <saravananalagarsamy@yahoo.com>
2025-08-14 22:26:10 -0700

    constant

---
+++ src/constantexample.py
@@ -0,0 +1,7 @@
+from typing import Final
+PROGRAM_NAME: Final = "Saka Learning"
+PROGRAM_VERSION: Final = "1.0.0"
+PROGRAM_AUTHOR: Final = "Saka"
+PROGRAM_DESCRIPTION: Final = "A program to demonstrate various Python data types and their usage."
+PROGRAM_LICENSE: Final = "MIT License"
+PROGRAM_COPYRIGHT: Final = "© 2023 Saka"
