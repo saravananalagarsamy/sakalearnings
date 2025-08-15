7c6172acc3dbc5c8d0f38680bf40105533645b30
sa ka <saravananalagarsamy@yahoo.com>
2025-08-14 22:33:06 -0700

    function with params

--- src/functioneample.py
+++ src/functioneample.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
-def print_current_datetime() -> None:
-    console_output = f"Current date and time: {datetime.now()}"
-    print(console_output)
+def print_current_datetime() -> str:
+    console_output = f"Current date and time: {datetime.now()}"    
+    return console_output
 
-print_current_datetime()    
+def hello_world(name: str) -> None:
+    print(f"Hello, {name}! Today is {print_current_datetime()}")
+
+hello_world(input("Enter your name :"))    
